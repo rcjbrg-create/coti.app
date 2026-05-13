26114c395ddac794ccb7945512351a4eecc0896b
Admin COTI <rcjbrg@gmail.com>
2026-05-13 15:50:39 -0300

    Disable middleware temporarily

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,8 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { updateSession } from "@/lib/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+  return NextResponse.next();
 }
 
 export const config = {
